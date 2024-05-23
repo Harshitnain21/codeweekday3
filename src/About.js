cb5247f16c90a0d783069bb93849e7d732ec2924
shashiweb <shashiweb64@gmail.com>
2024-05-23 16:07:01 +0530

    create new project

---
+++ src/About.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react'
+
+function About() {
+  return (
+    <h1>sdfsdfshjdfhsjkdhfjksdhf</h1>
+  )
+}
+
+export default About
+
+
+export function About1()
+{
+    return(
+        <Fragment>
+            <h1>sdjkfjhsjdfhjksdh fjsh fjh sddj fhdsf</h1>
+            <p>sjddfhjkshdfj</p>
+            <div>ppppppppppppppppppppnjjjjhf</div>
+        </Fragment>
+        );
+}
