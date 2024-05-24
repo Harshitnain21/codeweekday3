dc42eaff439795767a00cf0c0e66baa94aed8402
shashiweb <shashiweb64@gmail.com>
2024-05-24 17:08:05 +0530

    change

--- src/About.js
+++ src/About.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
-import "./style.css";
+import  {Button,Navbar,Container,Nav,NavDropdown}  from "react-bootstrap";
+
 
 function About() {
   return (
@@ -10,15 +11,84 @@ function About() {
 export default About
 
 
-export function About1()
-{
-    return(
-        <Fragment>
-          <div className='first'>
-            <h1>sdjkfjhsjdfhjksdh fjsh fjh sddj fhdsf</h1>
-            <p>sjddfhjkshdfj</p>
-            <div>ppppppppppppppppppppnjjjjhf</div>
-            </div>
-        </Fragment>
-        );
+export function About1() {
+  return (
+    <Fragment>
+      <div className='first'>
+        <h1>sdjkfjhsjdfhjksdh fjsh fjh sddj fhdsf</h1>
+        <p>sjddfhjkshdfj</p>
+        <div className='heading'>ppppppppppppppppppppnjjjjhf</div>
+      </div>
+
+      <div className='container border m-5 bg-danger text-center text-white'>
+        <div className='row'>
+          <div className='col-12'>
+            <p className='h1'>use of bootstrap </p>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+              <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                      <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                      <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                      </a>
+                      <ul className="dropdown-menu">
+                        <li><a className="dropdown-item" href="#">Action</a></li>
+                        <li><a className="dropdown-item" href="#">Another action</a></li>
+                        <li><hr className="dropdown-divider"/></li>
+                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                      </ul>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                  </ul>
+                  <form className="d-flex" role="search">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                      <button className="btn btn-outline-success" type="submit">Search</button>
+                  </form>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+          <h1>use of react bootstrap</h1>
+          <Button variant="primary">Primary</Button>
+          <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    </Fragment>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Xyz from './About';
 import { About1 } from './About';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
